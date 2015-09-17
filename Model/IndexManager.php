489184c2d871fe6601595a8d1dafe0fe73fc02d8
Genar Trias Ortiz <genar@acs.li>
2015-09-17 14:34:42 +0200

    Adding index when organization is saved

--- Model/IndexManager.php
+++ Model/IndexManager.php
@@ -2,6 +2,8 @@
 
 namespace Cirici\MatchAgainstBundle\Model;
 
+use Cirici\MatchAgainstBundle\Entity\SearchTextIndex;
+
 class IndexManager
 {
     private $em;
@@ -9,5 +11,21 @@ class IndexManager
     public function setEm($em)
     {
         $this->em = $em;
+    }
+
+    public function addIndex($entity, $field)
+    {
+        $index = new SearchTextIndex();
+        $index->setForeignId($entity->getId());
+        $index->setModel(get_class($entity));
+        $index->setField($field);
+        $gfield = 'get' . ucfirst($field);
+        $content = $entity->{$gfield}();
+        $index->setContent($content);
+
+        if ($index->getContent()) {
+            $this->em->persist($index);
+            $this->em->flush();
+        }
     }
 }
